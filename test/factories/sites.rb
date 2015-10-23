9fbc89727e9c701bba108eedada48820e9c5ad1f
Amin Ariana <aminka@gmail.com>
2015-10-22 18:03:44 -0700

    Annotate models

--- test/factories/sites.rb
+++ test/factories/sites.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+# Schema version: 20151023004631
+#
+# Table name: sites
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  rank        :integer
+#  url         :text
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryGirl.define do
   factory :site do |f|
     f.name "Google"
